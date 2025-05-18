d156997375f88cb90d85e0ac7f26271d2fad03c6
Oleg <kaspruk.oleg2002@gmail.com>
2025-05-18 09:51:15 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [tovary, setTovary] = useState<{id:number; content:string} []>([])
+
+  useEffect(()=>{
+    axios.get(`${process.env.REACT_APP_API_URL}`)
+      .then(res=>setTovary(res.data))
+      .catch(err=>console.error(err))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>{tovary.map(tov=>(
+        <li key={tov.id}>{tov.content}</li>
+      ))}</h1>
     </div>
   );
 }
